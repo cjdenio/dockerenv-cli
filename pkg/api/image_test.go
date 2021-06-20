1acc84308a41f8e87421035b3abc7a9ca26edc6c
Matt Gleich <git@mattglei.ch>
2021-06-19 23:31:50 -0400

    fix: update test
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- pkg/api/image_test.go
+++ pkg/api/image_test.go
@@ -11,17 +11,57 @@ func TestImage(t *testing.T) {
 	// Working image
 	result1, err := Image("postgres")
 	assert.NoError(t, err)
-	assert.Equal(
-		t,
-		result1,
-		ImageData{URL: "https://hub.docker.com/_/postgres", Variables: &[]struct {
-			Name        string
-			Description string
-			Default     string
-			Required    bool
-			Uncommon    bool
-		}{{Name: "POSTGRES_INITDB_ARGS", Description: "Used to send arguments to `postgres initdb`. The value is a space separated string of arguments as `postgres initdb` would expect them. This is useful for adding functionality like data page checksums: -e `POSTGRES_INITDB_ARGS=\"--data-checksums\"`", Default: "", Required: false, Uncommon: true}, {Name: "POSTGRES_INITDB_WALDIR", Description: "Used to define another location for the Postgres transaction log.", Default: "", Required: false, Uncommon: true}, {Name: "POSTGRES_HOST_AUTH_METHOD", Description: "Used to control the `auth-method` for `host` connections for `all` databases, `all` users, and `all` addresses.", Default: "", Required: false, Uncommon: true}, {Name: "POSTGRES_PASSWORD", Description: "Sets the password for the default user", Default: "", Required: true, Uncommon: false}, {Name: "POSTGRES_USER", Description: "The username for the default user", Default: "postgres", Required: false, Uncommon: false}, {Name: "POSTGRES_DB", Description: "The default database name. Defaults to the value of `POSTGRES_USER`", Default: "", Required: false, Uncommon: false}}},
-	)
+	assert.Equal(t, "https://hub.docker.com/_/postgres", result1.URL)
+	assert.Equal(t, []struct {
+		Name        string
+		Description string
+		Default     string
+		Required    bool
+		Uncommon    bool
+	}{
+		{
+			Name:        "POSTGRES_HOST_AUTH_METHOD",
+			Description: "Used to control the `auth-method` for `host` connections for `all` databases, `all` users, and `all` addresses.",
+			Default:     "",
+			Required:    false,
+			Uncommon:    true,
+		},
+		{
+			Name:        "POSTGRES_PASSWORD",
+			Description: "Sets the password for the default user",
+			Default:     "",
+			Required:    true,
+			Uncommon:    false,
+		},
+		{
+			Name:        "POSTGRES_USER",
+			Description: "The username for the default user",
+			Default:     "postgres",
+			Required:    false,
+			Uncommon:    false,
+		},
+		{
+			Name:        "POSTGRES_DB",
+			Description: "The default database name. Defaults to the value of `POSTGRES_USER`",
+			Default:     "",
+			Required:    false,
+			Uncommon:    false,
+		},
+		{
+			Name:        "POSTGRES_INITDB_ARGS",
+			Description: "Used to send arguments to `postgres initdb`. The value is a space separated string of arguments as `postgres initdb` would expect them. This is useful for adding functionality like data page checksums: -e `POSTGRES_INITDB_ARGS=\"--data-checksums\"`",
+			Default:     "",
+			Required:    false,
+			Uncommon:    true,
+		},
+		{
+			Name:        "POSTGRES_INITDB_WALDIR",
+			Description: "Used to define another location for the Postgres transaction log.",
+			Default:     "",
+			Required:    false,
+			Uncommon:    true,
+		}},
+		*result1.Variables)
 
 	// Testing failing case
 	_, err = Image("fakeimage")
